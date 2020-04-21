66942ac1e69eec23a68200d8597246b316105c68
Mirat Rakhmanau <33043979+Miratminsk@users.noreply.github.com>
2020-04-21 13:47:04 -0500

    2. Перегрузка конструктора. [#173890]

--- src/main/java/ru/job4j/oop/Animal.java
+++ src/main/java/ru/job4j/oop/Animal.java
@@ -2,8 +2,13 @@ package ru.job4j.oop;
 
 public class Animal {
 
+    private String name;
+
     public Animal() {
-        super();
+    }
+
+    public Animal(String name) {
+        this.name = name;
         System.out.println("Animal class");
     }
 
--- src/main/java/ru/job4j/oop/Predator.java
+++ src/main/java/ru/job4j/oop/Predator.java
@@ -3,7 +3,7 @@ package ru.job4j.oop;
 public class Predator extends Animal {
 
     public Predator() {
-        super();
+        super("Rex");
         System.out.println("Predator class");
     }
 }
