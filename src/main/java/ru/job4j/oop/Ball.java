66b0d6f5ddb6ddd9836c9f283c6669a6815f8dce
Mirat Rakhmanau <33043979+Miratminsk@users.noreply.github.com>
2020-04-21 10:10:34 -0500

    1.6. Взаимодействие объектов. [#166087]

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class Ball {
+
+    private String name;
+
+    public Ball() {
+        this.name = "Mirat";
+    }
+
+    public String getName() {
+        return name;
+    }
+}
