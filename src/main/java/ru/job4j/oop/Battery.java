d9302a3bc002bc1c07612167ac746967bf764a8b
Mirat Rakhmanau <33043979+Miratminsk@users.noreply.github.com>
2020-04-21 10:28:37 -0500

    1.7. Состояние объекта. [#167388]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,25 @@
+package ru.job4j.oop;
+
+public class Battery {
+
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public void exchange(Battery another) {
+        another.load += this.load;
+        this.load = 0;
+        System.out.println("Full battery load is " + this.load + ".");
+        System.out.println("Empty battery load is " + another.load + ".");
+    }
+
+    public static void main(String[] args) {
+        Battery fullBattery = new Battery(100);
+        Battery emptyBattery = new Battery(0);
+        System.out.println("Full battery load is " + fullBattery.load + ".");
+        System.out.println("Empty battery load is " + emptyBattery.load + ".");
+        fullBattery.exchange(emptyBattery);
+    }
+}
