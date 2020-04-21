d2836eb69c7217e4f50e683c5eff3bd1a1c81980
Mirat Rakhmanau <33043979+Miratminsk@users.noreply.github.com>
2020-04-21 09:26:44 -0500

    1.1.1. Конструктор по умолчанию. [#173889]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -3,26 +3,35 @@ package ru.job4j.tracker;
 import java.util.Objects;
 
 public class Item {
-    private String id;
-    private String name;
 
-    public Item(String name) {
-        this.name = name;
+    public Item() {
+        super();
+        System.out.println("load item");
     }
 
-    public String getId() {
-        return id;
+    public static void main(String[] args) {
+        Bug bug = new Bug();
     }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+//    private String id;
+//    private String name;
+//
+//    public Item(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 }
