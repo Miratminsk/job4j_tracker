e2cf67cea6a330b5f4b5c1bf8cd9120d9a76e1ef
Mirat Rakhmanau <33043979+Miratminsk@users.noreply.github.com>
2020-04-24 16:36:21 -0500

    0.3. Тестирование исключений с junit [#219366]

---
+++ chapter_002/src/test/java/job4j/ru/job4j/ex/FactTest.java
@@ -0,0 +1,21 @@
+package job4j.ru.job4j.ex;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FactTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void WhenFactorialNegativeNumberThenError() {
+        new Fact().calc(-1);
+    }
+
+    @Test
+    public void WhenFactorial5Than120Test() {
+        assertEquals(120, new Fact().calc(5));
+    }
+
+
+
+}
